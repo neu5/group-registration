c4e3d9c8919423eda0a6ba8ddcb0736ee1fbb970
neu5 <neus90@gmail.com>
2020-02-26 22:10:42 +0100

    unfinished #5: stworzenie formularza w react

--- form/src/components/Form.js
+++ form/src/components/Form.js
@@ -10,7 +10,11 @@ import styles from './Form.css';
 
 const CHECKBOX_STATES = {
   isInstitution: 'isInstitution',
-  isLeader: 'isLeader'
+  isLeader: 'isLeader',
+  isPTTK: 'isPTTK',
+  isTShirt: 'isTShirt',
+  isTransport: 'isTransport',
+  isRegulationsAccepted: 'isRegulationsAccepted'
 };
 
 const routes = [
@@ -37,11 +41,63 @@ const hours = [
   }
 ];
 
+const tShirtKind = [
+  {
+    key: 'damska',
+    text: 'damska',
+    value: 'damska'
+  }, {
+    key: 'męska',
+    text: 'męska',
+    value: 'męska'
+  }
+];
+
+const tShirtsSizes = [
+  {
+    key: '1 godzina',
+    text: '1 godzina',
+    value: '1 godzina'
+  }, {
+    key: '2 godzina',
+    text: '2 godzina',
+    value: '2 godzina'
+  }
+];
+
+const destination = [
+  {
+    key: '1 godzina',
+    text: '1 godzina',
+    value: '1 godzina'
+  }, {
+    key: '2 godzina',
+    text: '2 godzina',
+    value: '2 godzina'
+  }
+];
+
+const departureTime = [
+  {
+    key: '1 godzina',
+    text: '1 godzina',
+    value: '1 godzina'
+  }, {
+    key: '2 godzina',
+    text: '2 godzina',
+    value: '2 godzina'
+  }
+];
+
 class App extends Component {
   state = { 
     name: '', 
     [CHECKBOX_STATES.isInstitution]: false,
-    [CHECKBOX_STATES.isLeader]: false 
+    [CHECKBOX_STATES.isLeader]: false,
+    [CHECKBOX_STATES.isPTTK]: false,
+    [CHECKBOX_STATES.isTShirt]: false,
+    [CHECKBOX_STATES.isTransport]: false,
+    [CHECKBOX_STATES.isRegulationsAccepted]: false
   };
 
   toggle = (prop) => this.setState((prevState) => ({ [prop]: !prevState[prop] }));
@@ -57,23 +113,23 @@ class App extends Component {
         </p>
         <Segment>
           <Form>
-            <Form.Input 
-              label='Nazwa drużyny' 
+            <Form.Input
+              label='Nazwa drużyny'
               placeholder='Nazwa drużyny'
             />
-            <Form.Checkbox 
-              label='Czy instytucja?' 
+            <Form.Checkbox
+              label='Czy instytucja?'
               onChange={() => {this.toggle(CHECKBOX_STATES.isInstitution)}}
               checked={this.state[CHECKBOX_STATES.isInstitution]}
             />
             {this.state[CHECKBOX_STATES.isInstitution] && (
               <>
-                <Form.Input 
-                  label='Nazwa instytucji' 
+                <Form.Input
+                  label='Nazwa instytucji'
                   placeholder='Nazwa instytucji'
                 />
-                <Form.Checkbox 
-                  label='Czy są opiekunowie?' 
+                <Form.Checkbox
+                  label='Czy są opiekunowie?'
                   onChange={() => {this.toggle(CHECKBOX_STATES.isLeader)}}
                   checked={this.state[CHECKBOX_STATES.isLeader]}
                 />
@@ -82,28 +138,102 @@ class App extends Component {
                 )}
               </>
             )}
-            <Form.Dropdown 
+            <Form.Dropdown
               placeholder='Wybierz trasę'
               selection
               options={routes}
             />
-            <Form.Dropdown 
+            <Form.Dropdown
               placeholder='Planowana godzina startu'
               selection
               options={hours}
             />
-            <Form.Input 
-              label='Kierownik drużyny' 
+            <Form.Input
+              label='Kierownik drużyny'
               placeholder='Imię i nazwisko'
             />
-            <Form.Input 
-              label='Adres email' 
+            <Form.Input
+              label='Adres email'
               placeholder='Adres email'
             />
-            <Form.Input 
-              label='Numer telefonu' 
+            <Form.Input
+              label='Numer telefonu'
               placeholder='Numer telefonu'
             />
+            <Segment>
+              <Header as='h1'>
+                Uczestnicy
+              </Header>
+              <Button positive>Dodaj uczestnika +</Button>
+              <Form.Input
+                label='Imię i nazwisko'
+                placeholder='Imię i nazwisko'
+              />
+              <Form.Input
+                label='Data urodzenia'
+                placeholder='Data urodzenia'
+              />
+              <Form.Checkbox
+                label='Czy jest członkiem PTTK?'
+                onChange={() => {this.toggle(CHECKBOX_STATES.isPTTK)}}
+                checked={this.state[CHECKBOX_STATES.isPTTK]}
+              />
+              {this.state.isPTTK && (
+                <Form.Input
+                  label='Nr legitymacji PTTK'
+                  placeholder='Nr legitymacji PTTK'
+                />
+              )}
+              <Form.Checkbox
+                label='Koszulka?'
+                onChange={() => {this.toggle(CHECKBOX_STATES.isTShirt)}}
+                checked={this.state[CHECKBOX_STATES.isTShirt]}
+              />
+              {this.state.isTShirt && (
+                <>
+                  <Form.Dropdown
+                    placeholder='Rodzaj'
+                    selection
+                    options={tShirtKind}
+                  />
+                  <Form.Dropdown
+                    placeholder='Rozmiar'
+                    selection
+                    options={tShirtsSizes}
+                  />
+                </>
+              )}
+              <Form.Checkbox
+                label='Transport?'
+                onChange={() => {this.toggle(CHECKBOX_STATES.isTransport)}}
+                checked={this.state[CHECKBOX_STATES.isTransport]}
+              />
+              {this.state.isTransport && (
+                <>
+                  <Form.Dropdown
+                    placeholder='Cel'
+                    selection
+                    options={destination}
+                  />
+                  <Form.Dropdown
+                    placeholder='Godzina odjazdu'
+                    selection
+                    options={departureTime}
+                  />
+                </>
+              )}
+              <Button negative>Usuń uczestnika -</Button>
+              <Button positive>Dodaj uczestnika +</Button>
+            </Segment>
+            <Form.TextArea placeholder='Uwagi'></Form.TextArea>
+            <Form.Checkbox
+              label='Akceptuję regulamin'
+              onChange={() => {this.toggle(CHECKBOX_STATES.isRegulationsAccepted)}}
+              checked={this.state[CHECKBOX_STATES.isRegulationsAccepted]}
+            />
+            <Segment>
+              Captcha
+            </Segment>
             <Button primary>
               Wyślij
             </Button>
